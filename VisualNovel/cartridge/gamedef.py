237cccfba7cc13f58418f79a5b9834a06ed3781c
wkta <thomas.iw@kata.games>
2024-11-01 01:49:55 +0100

    cleanup demo VisualNovel (befor switching to actor-based)

--- VisualNovel/cartridge/gamedef.py
+++ VisualNovel/cartridge/gamedef.py
@@ -1,115 +1,59 @@
-from .glvars import pyv
-pyv.bootstrap_e()
-
-from .story import ConversationView
 from . import glvars
-from .story import Automaton
+from .glvars import pyv
 
 
-# - gl variables
+pyv.bootstrap_e()
 EngineEvTypes = pyv.EngineEvTypes
 conv_viewer = None
-# - new gl vars
-# global variables
-evm = None
 msg = None
 
 
-# ----------------------
-# Define controllers etc
-# --------------------------------------------
 class BasicCtrl(pyv.EvListener):
-
     def on_quit(self, ev):
         pyv.vars.gameover = True
 
-    # def proc_event(self, gdi):
-    #     global conversation_ongoing, map_viewer, mypc, current_tilemap, current_path
-    #     if gdi.type == pygame.KEYDOWN:
-    #         if gdi.key == pygame.K_ESCAPE:
-    #             if conversation_ongoing:
-    #                 # abort
-    #                 self.pev(MyEvTypes.ConvEnds)
-    #             else:
-    #                 self.pev(EngineEvTypes.GAMEENDS)
-    #         elif gdi.key == pygame.K_d and mypc.x < tilemap_width - 1.5:
-    #             mypc.x += 0.1
-    #         elif gdi.key == pygame.K_a and mypc.x > -1:
-    #             mypc.x -= 0.1
-    #         elif gdi.key == pygame.K_w and mypc.y > -1:
-    #             mypc.y -= 0.1
-    #         elif gdi.key == pygame.K_s and mypc.y < tilemap_height - 1.5:
-    #             mypc.y += 0.1
-    #         elif gdi.key == pygame.K_TAB:
-    #             current_tilemap = 1 - current_tilemap
-    #             map_viewer.switch_map(maps[current_tilemap])
+    def on_keydown(self, ev):
+        if ev.key == pyv.evsys0.K_ESCAPE:
+            pyv.vars.gameover = True
 
 
 class PathCtrl(pyv.EvListener):
-    def __init__(self):
-        super().__init__()
+    def __init__(self): super().__init__()
 
     def start_conv(self):
         global conv_viewer
+        from .story import Automaton
+        from .story import ConversationView
         glvars.conversation_ongoing = True
-
-        # TODO faire en sorte que l'automate prenne l'interface de dialogue.Offer
-        # ou inversement, que la ConversationView puisse s'adapter à une interface type AUtomaton
-
-        # myconvo = dialogue.Offer.from_json(pyv.vars.data['legacy_conv'])  # using data pre-loaded thu pyv
-        conv_viewer = ConversationView(Automaton(
-            ('encounter_1', pyv.vars.data['encounter_1']),
-            ('encounter_Y', pyv.vars.data['encounter_Y']),
-        ))
+        conv_viewer = ConversationView(
+            Automaton(('encounter_1', pyv.vars.data['encounter_1']),
+                      ('encounter_Y', pyv.vars.data['encounter_Y'])))
         conv_viewer.turn_on()
 
     def on_conv_finish(self, ev):
-        glvars.conversation_ongoing = False  # unlock player movements
+        glvars.conversation_ongoing = False
         if conv_viewer.active:
             conv_viewer.turn_off()
 
 
 def init(vmst=None):
-    global evm, msg
-    pyv.init(1) #pyv.LOW_RES_MODE, wcaption='IsometricRPG Tech Demo')
-    evm = pyv.get_ev_manager()
-    evm.setup()
-
+    global msg
+    pyv.init(pyv.LOW_RES_MODE)
+    pyv.get_ev_manager().setup()
     pctrl = PathCtrl()
     pctrl.turn_on()
     bctrl = BasicCtrl()
     bctrl.turn_on()
-    
-    # start conv
     pctrl.start_conv()
     msg = pyv.new_font_obj(None, 33).render('encounter ended', False, 'orange')
 
-    #gctrl = pyv.get_game_ctrl()
-    #gctrl.turn_on()
-
 
 def update(time_info=None):
-    # for ev in pygame.event.get():
-    #     if ev.type == pygame.QUIT:
-    #         pyv.vars.gameover = True
-    #     elif ev.type == pygame.KEYDOWN:
-    #         kpressed.add(ev.key)
-    #     elif ev.type == pygame.KEYUP:
-    #         kpressed.remove(ev.key)
-
-    # - logic
+    evm = pyv.get_ev_manager()
     evm.post(EngineEvTypes.Update, info_t=time_info)
-
-    # do not draw directly, use events!
-    # scr = pyv.vars.screen
-
-    # scr.fill('paleturquoise3')
-    # if len(kpressed):
-    #     pyv.draw_rect(scr, 'orange', (15, 64, 100, 100))
     pyv.vars.screen.fill(pyv.pal.punk.nightblue)
     evm.post(EngineEvTypes.Paint, screen=pyv.vars.screen)
-    evm.update()  # auto convert pygame events to high-level events
-
+    evm.update()  # note that this auto-converts pygame events to higher-level events
     if not glvars.conversation_ongoing:
         pyv.vars.screen.blit(msg, (256, 320))
     pyv.flip()
@@ -117,4 +61,4 @@ def update(time_info=None):
 
 def close(vmst=None):
     pyv.close_game()
-    print('gameover!')
+    print('visual novel:over')
